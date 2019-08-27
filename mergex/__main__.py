d034d0d985b0cdf6451249c9b77a026b2acc9777
Luis Monteiro <luiscarlos.monteiro@altran.com>
2019-08-27 14:57:20 +0100

    update: added native code

--- mergex/__main__.py
+++ mergex/__main__.py
@@ -1,2 +1,33 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+from git      import Repo
+from sys      import argv
+from argparse import ArgumentParser
+from .native  import format
+# -----------------------------------------------------------------------------
+# merge
+# -----------------------------------------------------------------------------
+def merge(current, base, other):
+    try:
+        format(current)
+        format(base)
+        format(other)
+        return Repo().git.merge_file(current, base, other)
+    except Exception as e:
+        print(e)
+    return 255
+# -----------------------------------------------------------------------------
+# main
+# -----------------------------------------------------------------------------
 def main():
-    print("hello world!!!!")
+    parser = ArgumentParser()
+    parser.add_argument('current', type=str, default = '')
+    parser.add_argument('base',    type=str, default = '')
+    parser.add_argument('other',   type=str, default = '')
+    args = parser.parse_args()
+    return merge(args.current, args.base, args.other)
+# -----------------------------------------------------------------------------
+# end
+# -----------------------------------------------------------------------------
